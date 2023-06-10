a2b93c0fafa6db1000763592a1666e358c64c582
Sanjukta Roy <sanjuktaroy2001@gmail.com>
2023-06-10 18:56:43 +0530

    final commit 1

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ app.get("/updatenote", (req,res) => {
     })
 });
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
     console.log("Server listening on PORT 3001...");
 });
 
